b9240eacdd6b1bcac65ad352b94098b7d97022f0
Kah-los <kahloscase@gmail.com>
2025-09-20 12:08:11 +0200

    Created About page

---
+++ page/About.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Carlos Adabe"
+)
+st.markdown(
+"""
+The final project aims to apply data science concepts and skills on a medical case study that you and your team select from a public data source.
+The project assumes that you bring the technical Python skills from previous courses (DSHI: Data Science for Health Informatics),
+as well as the analytical skills to argue how and why specific techniques could enhance the problem domain related to the selected dataset.
+
+"""
+)
